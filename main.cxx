531315ab5df3920f1a0811e01301dfc1cb02fe34
Ossian Malmborg <os2287ma-s@student.lu.se>
2025-10-20 11:57:22 +0200

    First assignment

--- main.cxx
+++ main.cxx
@@ -4,11 +4,31 @@
  * */
 
 #include "as1.hpp"
+#include "as2.hpp"
 #include <iostream>
 
 using namespace homework;
 
 int main() { 
-  
+//as1
+std::cout << "Assignment 1:" << std::endl;
+
+int a = 1;
+int b = 2;
+int c = 3;  
+
+std::cout << "Before the swap: (1,2,3)" << std::endl;
+homework::swap_ptr(&a, &b, &c);
+std::cout << "After the swap: (" << a << ", " << b << ", " << c << ")" << std::endl;
+
+int d = 37;
+std::cout << "My value is: " << d << std::endl;
+homework::UniqueData mydata(d);
+int val = mydata.get();
+std::cout << "My data value is now: " << mydata.get() << std::endl;
+mydata.set(73);
+std::cout << "My data value is now: " << mydata.get() << std::endl;
+val = mydata.get();
+
 }
 
--- src/as1.cxx
+++ src/as1.cxx
@@ -2,8 +2,22 @@
 
 namespace homework {
 
-void swap_ptr(int* a, int* b, int *c) { return;}
+void swap_ptr(int* a, int* b, int *c) {
+    int old_a = *a;
+    *a = *b;
+    *b = *c;
+    *c = old_a;
+}
 
+UniqueData::UniqueData(int value)
+    : data_{std::make_unique<int>(value)} {}
 
+int UniqueData::get() const {
+    return* data_;
+}
+
+void UniqueData::set(int value) {
+    *data_ = value;
+}
 
 }; // namespace homework
